0223d437b6142cd4318ac948138586ffb1c1224f
Flashdevnak <26nak.testdev@gmail.com>
2025-04-06 09:10:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ function renderTable(data) {
 
     tr.innerHTML = `
       <td>${row['上一站网点名称 สาขาก่อนหน้า']}</td>
+      <td>${row['车线名称 ชื่อเส้นทางการเดินรถ']}</td>
       <td>${row['司机姓名 ชื่อพนักงานขับรถ']}</td>
       <td>${row['司机电话 เบอร์โทรพนักงานขับรถ']}</td>
       <td>${row['车辆类型 ประเภทรถ']}</td>
@@ -72,6 +73,7 @@ function renderCards(data) {
     card.className = "card";
     card.innerHTML = `
       <div class="card-item"><span>สาขาก่อนหน้า:</span> ${row['上一站网点名称 สาขาก่อนหน้า']}</div>
+      <div class="card-item"><span>สาขาก่อนหน้า:</span> ${row['车线名称 ชื่อเส้นทางการเดินรถ']}</div>
       <div class="card-item"><span>ชื่อพนักงาน:</span> ${row['司机姓名 ชื่อพนักงานขับรถ']}</div>
       <div class="card-item"><span>เบอร์โทร:</span> ${row['司机电话 เบอร์โทรพนักงานขับรถ']}</div>
       <div class="card-item"><span>ประเภทรถ:</span> ${row['车辆类型 ประเภทรถ']}</div>
